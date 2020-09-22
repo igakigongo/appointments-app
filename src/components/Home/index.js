33f38dd7db9800ab84ae8fa2ffb546b05835636e
Edward Iga Kigongo <igakigongo@gmail.com>
2020-09-23 00:59:51 +0200

    complete styling the sidebar and home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,9 +4,9 @@ import './styles.scss';
 
 const Home = () => <>
   <div className="home-page">
-    <h1 className="font-bold my-8 text-green-400 text-5xl">Doctor's Appointment Booking App</h1>
+    <h1 className="font-bold my-8 text-green-600 text-5xl">Doctor's Appointment Booking App</h1>
     <Link 
-      className="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full"
+      className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-full shadow-2xl"
       to='/'>
       Book an Appointment
     </Link>
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,31 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './styles.scss';
 
-const SideBar = () => (
-  <div className="sidebar border-r-2 border-gray-300 shadow-2xl">
-    <div className="brand">Brand</div>
-    <div className="nav">
-      <h1>Side Bar</h1>
-      <ul>
-        <li><Link to='/'>About</Link></li>
-        <li><Link to='/'>Appointments</Link></li>
-        <li><Link to='/'>Login</Link></li>
-        <li><Link to='/'>Sign In</Link></li>
-        <li><Link to='/'>Sign Out</Link></li>
-      </ul>
-      {/* <ul>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">My Appointments</a></li>
-        <li><a href="#">Login</a></li>
-        <li><a href="#">Sign In</a></li>
-        <li><a href="#">Sign Out</a></li>
-      </ul> */}
+const getClass = (defaultClass, currentPath) => testPath => currentPath === testPath ?
+  `${defaultClass} active` : defaultClass;
+
+const SideBar = () => {
+  const defaultLinkClass = "p-3 font-bold pl-6 hover:bg-green-500";
+  const { pathname } = useLocation();
+
+  const isActive = getClass(defaultLinkClass, pathname);
+
+  return (
+    <div className="sidebar bg-gray-100 flex flex-col justify-between shadow-2xl">
+      <div className="brand flex-none p-10">
+        <img
+          alt="brand"
+          className="ml-auto mr-auto"
+          src="https://img.icons8.com/ios-filled/100/000000/person-calendar.png" />
+      </div>
+      <div className="nav flex-1">
+        <ul className="ml-8 uppercase">
+          <li className={isActive('/')}><Link to='/'>About</Link></li>
+          <li className={isActive('/appointments')}><Link to='/'>Appointments</Link></li>
+          <li className={isActive('/login')}><Link to='/'>Login</Link></li>
+          <li className={isActive('/signIn')}><Link to='/'>Sign In</Link></li>
+          <li className={isActive('/signOut')}><Link to='/'>Sign Out</Link></li>
+        </ul>
+      </div>
+      <div className="flex-none social-icons">
+        <ul className="flex justify-center my-3">
+          <li><i className="fa fa-twitter px-2"></i></li>
+          <li><i className="fa fa-facebook-f px-2"></i></li>
+          <li><i className="fa fa-google-plus px-2"></i></li>
+          <li><i className="fa fa-vimeo px-2"></i></li>
+          <li><i className="fa fa-pinterest-p px-2"></i></li>
+        </ul>
+        <p className="mb-3 font-semibold text-center text-xs">
+          &copy; {new Date().getFullYear()} - Edward Iga Kigongo
+        </p>
+      </div>
     </div>
-    <div className="social-icons">
-      Social Icons
-    </div>
-  </div>
-);
+  );
+};
 
 export default SideBar;
