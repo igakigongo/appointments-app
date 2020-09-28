3506037c0d94a4dc931e1c3c705fd81e6c203e49
Edward Iga Kigongo <igakigongo@gmail.com>
2020-09-28 13:05:52 +0200

    use json as default content type for payloads transmitted to the server

--- src/redux/async-actions.js
+++ src/redux/async-actions.js
@@ -6,8 +6,9 @@ const makeHttpRequest = (url, options = {}) => {
   const token = getAuthToken();
   const { body, contentType, method } = options;
   return fetch(url, {
-    body: body,
+    body: body ? JSON.stringify(body) : null,
     headers: {
+      'Authorization': token ? `Bearer ${token}` : null,
       'Content-Type': contentType || 'application/json;charset=UTF-8',
     },
     method: method || 'GET'
@@ -16,12 +17,12 @@ const makeHttpRequest = (url, options = {}) => {
 
 export const signIn = (credentials) => () => makeHttpRequest(`${API_URL}/auth/login`,
   {
-    body: JSON.stringify(credentials),
+    body: credentials,
     method: 'POST'
   });
 
 export const signUp = (userInfo) => () => makeHttpRequest(`${API_URL}/signup`,
   {
-    body: JSON.stringify(userInfo),
+    body: userInfo,
     method: 'POST'
   });
