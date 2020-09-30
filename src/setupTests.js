46ca37899618b76e67781dbc96f57010be4d770d
Edward Iga Kigongo <igakigongo@gmail.com>
2020-10-01 01:48:27 +0200

    add mutation observer mock in jest

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+const mutationObserverMock = jest.fn(function MutationObserver(callback) {
+  this.observe = jest.fn();
+  this.disconnect = jest.fn();
+  // Optionally add a trigger() method to manually trigger a change
+  this.trigger = (mockedMutationsList) => {
+      callback(mockedMutationsList, this);
+  };
+});
+
+global.MutationObserver = mutationObserverMock;
